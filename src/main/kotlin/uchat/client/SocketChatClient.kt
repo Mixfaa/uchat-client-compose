23629ee97e9da3a83ff5ce9c05d1ac6f1ec44850
Миша Език <mishunder9354@gmail.com>
2024-02-02 23:29:03 +0200

    switched_to_secure_sockets

--- src/main/kotlin/uchat/client/SocketChatClient.kt
+++ src/main/kotlin/uchat/client/SocketChatClient.kt
@@ -4,38 +4,52 @@ import kotlinx.coroutines.*
 import uchat.message.transactions.TransactionBase
 import uchat.message.transactions.deserializeTransaction
 import uchat.misc.Utils
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.OutputStream
 import java.net.InetAddress
-import java.net.Socket
+import java.security.KeyStore
+import java.security.SecureRandom
+import javax.net.ssl.*
 
 abstract class SocketChatClient(
     address: String,
     port: Int,
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 ) : AutoCloseable {
-    private val socket = Socket(InetAddress.getByName(address), port)
+    private val socket: SSLSocket
+    private val outputStream: OutputStream
+    private val inputReader: BufferedReader
 
     init {
+        val keyStore = KeyStore.getInstance("JKS")
+        keyStore.load(FileInputStream(KEYSTORE_PATH), KEYSTORE_PASSWORD.toCharArray())
+
+        val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+        keyManagerFactory.init(keyStore, KEYSTORE_PASSWORD.toCharArray())
+
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(keyStore)
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(keyManagerFactory.keyManagers, trustManagerFactory.trustManagers, SecureRandom())
+
+        val sslSocketFactory = sslContext.socketFactory as SSLSocketFactory
+        socket = sslSocketFactory.createSocket(InetAddress.getByName(address), port) as SSLSocket
+
+        try {
+            socket.startHandshake()
+        } catch (ex: Exception) {
+            throw RuntimeException(ex)
+        }
+        outputStream = socket.outputStream
+        inputReader = socket.inputStream.bufferedReader()
+        socket.tcpNoDelay = true
+
         coroutineScope.launch {
-            val inputStream = socket.getInputStream()
-            val messageBuilder = StringBuilder()
             while (!socket.isClosed) {
                 try {
-                    var lastDelimiterIndex: Int
-                    do {
-                        var availableToRead = 0
-                        while (availableToRead <= 0) {
-                            availableToRead = inputStream.available()
-                            delay(50)
-                        }
-
-                        messageBuilder.append(String(inputStream.readNBytes(availableToRead)))
-                        lastDelimiterIndex = messageBuilder.lastIndexOf('\n')
-                    } while (lastDelimiterIndex == -1)
-
-                    val message = messageBuilder.substring(0, lastDelimiterIndex + 1)
-                    val cleared = messageBuilder.removeRange(0, lastDelimiterIndex + 1)
-                    messageBuilder.clear()
-                    messageBuilder.append(cleared)
+                    val message = inputReader.readLine()
 
                     for (json in Utils.splitJsons(message)) {
                         val transaction = deserializeTransaction(json).getOrNull()
@@ -54,11 +68,17 @@ abstract class SocketChatClient(
     abstract fun handleTransaction(transaction: TransactionBase)
 
     fun sendRequest(request: TransactionBase) = coroutineScope.launch {
-        socket.getOutputStream().write(request.serialized)
+        outputStream.write(request.serialized)
     }
 
     override fun close() {
         coroutineScope.cancel()
         socket.close()
+    }
+
+    companion object {
+        private const val KEYSTORE_PATH =
+            "C:\\Users\\mishu\\programming_projects\\java_kotlin\\uchat-client-compose\\keystore\\uchat-keystore.jks"
+        private const val KEYSTORE_PASSWORD = "semnadcat"
     }
 }
